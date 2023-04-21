2d9259e9309569ff3ec7cdd9d03750abfe036bcb
Farman Ali <farmanali494917@gmail.com>
2023-04-21 21:54:17 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   images: {
     domains: ["via.placeholder.com", "picsum.photos", "kt-storage.s3.amazonaws.com"],
   },
