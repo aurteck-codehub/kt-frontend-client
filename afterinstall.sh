e5544f9485ca6f866976aabfd148099dd7d4e5de
Farman Ali <farmanali494917@gmail.com>
2023-04-21 22:36:25 +0500

    Update afterinstall.sh

--- afterinstall.sh
+++ afterinstall.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-sudo apt-get install git -y
-git init
 export NVM_DIR="/root/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 cd /usr/share/nginx/html
+/root/.nvm/versions/node/v18.16.0/bin/sudo apt-get install git -y
+/root/.nvm/versions/node/v18.16.0/bin/git init
 /root/.nvm/versions/node/v18.16.0/bin/npm install
 /root/.nvm/versions/node/v18.16.0/bin/npm install next
 /root/.nvm/versions/node/v18.16.0/bin/npm install -g sass
