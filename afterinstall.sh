d7f1f995985c2516ab70c2b8e2051e964d166896
Farman Ali <farmanali494917@gmail.com>
2023-04-21 21:32:24 +0500

    Update afterinstall.sh

--- afterinstall.sh
+++ afterinstall.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
+export NVM_DIR="/root/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 cd /usr/share/nginx/html
 /root/.nvm/versions/node/v18.16.0/bin/npm install
-/root/.nvm/versions/node/v18.16.0/bin/npm run build
 /root/.nvm/versions/node/v18.16.0/bin/npm install next
 /root/.nvm/versions/node/v18.16.0/bin/npm install -g sass
 /root/.nvm/versions/node/v18.16.0/bin/npm run dev
-yarn dev
 
 
