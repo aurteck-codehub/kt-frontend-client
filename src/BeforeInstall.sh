6cca5de0f0de48dcef7cac809cf0eae24fda0655
Farman Ali <farmanali494917@gmail.com>
2023-04-22 17:27:47 +0500

    Update BeforeInstall.sh

--- src/BeforeInstall.sh
+++ src/BeforeInstall.sh
@@ -4,7 +4,5 @@ npm install husky --save-dev
 sudo apt-get install git -y
 git init
 npm run prepare
-sudo apt-get install npm
 sudo chown -R $(whoami) ~/.npm
-sudo apt-get install next -y
 sudo npm install -g sass
