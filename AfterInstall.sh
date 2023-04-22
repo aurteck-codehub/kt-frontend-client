8a8a68dada3514e07820fb5d4de3069be1918261
Farman Ali <farmanali494917@gmail.com>
2023-04-22 15:31:06 +0500

    Update AfterInstall.sh

--- AfterInstall.sh
+++ AfterInstall.sh
@@ -6,6 +6,9 @@ export NVM_DIR="/root/.nvm"
 /root/.nvm/versions/node/v18.16.0/bin/sudo apt-get install git -y
 /root/.nvm/versions/node/v18.16.0/bin/npm run prepare
 /root/.nvm/versions/node/v18.16.0/bin/git init
+/root/.nvm/versions/node/v18.16.0/bin/next dev
+/root/.nvm/versions/node/v18.16.0/bin/next build
+/root/.nvm/versions/node/v18.16.0/bin/next dev
 /root/.nvm/versions/node/v18.16.0/bin/npm run build
 /root/.nvm/versions/node/v18.16.0/bin/npm install next
 /root/.nvm/versions/node/v18.16.0/bin/npm install -g sass
