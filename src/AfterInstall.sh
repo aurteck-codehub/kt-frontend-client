6d178e68a5f5b2457b025bf2d6c00b9c126ba13f
Farman Ali <farmanali494917@gmail.com>
2023-04-22 17:03:35 +0500

    Update AfterInstall.sh

--- src/AfterInstall.sh
+++ src/AfterInstall.sh
@@ -4,15 +4,15 @@ export NVM_DIR="/root/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 sudo chown -R ubuntu:ubuntu /root/.nvm   
 sudo chmod -R 755 /root/.nvm
-/root/.nvm/versions/node/v18.16.0/bin/npm install husky --save-dev
-/root/.nvm/versions/node/v18.16.0/bin/sudo apt-get install git -y
-/root/.nvm/versions/node/v18.16.0/bin/npm run prepare
-/root/.nvm/versions/node/v18.16.0/bin/git init
-/root/.nvm/versions/node/v18.16.0/bin/next dev
-/root/.nvm/versions/node/v18.16.0/bin/next build
-/root/.nvm/versions/node/v18.16.0/bin/next start
-/root/.nvm/versions/node/v18.16.0/bin/npm run build
-/root/.nvm/versions/node/v18.16.0/bin/chmod +x next
-/root/.nvm/versions/node/v18.16.0/bin/npm install -g sass
-/root/.nvm/versions/node/v18.16.0/bin/npm run build
-/root/.nvm/versions/node/v18.16.0/bin/npm run dev
+sudo /root/.nvm/versions/node/v18.16.0/bin/npm install husky --save-dev
+sudo /root/.nvm/versions/node/v18.16.0/bin/apt-get install git -y
+sudo /root/.nvm/versions/node/v18.16.0/bin/npm run prepare
+sudo /root/.nvm/versions/node/v18.16.0/bin/git init
+sudo /root/.nvm/versions/node/v18.16.0/bin/next dev
+sudo /root/.nvm/versions/node/v18.16.0/bin/next build
+sudo /root/.nvm/versions/node/v18.16.0/bin/next start
+sudo /root/.nvm/versions/node/v18.16.0/bin/npm run build
+sudo /root/.nvm/versions/node/v18.16.0/bin/chmod +x next
+sudo /root/.nvm/versions/node/v18.16.0/bin/npm install -g sass
+sudo /root/.nvm/versions/node/v18.16.0/bin/npm run build
+sudo /root/.nvm/versions/node/v18.16.0/bin/npm run dev
