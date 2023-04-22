5593f0c71f853d73680b64391f32e2d49dee14aa
Farman Ali <farmanali494917@gmail.com>
2023-04-22 16:22:52 +0500

    Update BeforeInstall.sh

--- src/BeforeInstall.sh
+++ src/BeforeInstall.sh
@@ -3,7 +3,7 @@ cd /var/www/html/production
 npm install husky --save-dev
 sudo apt-get install git -y
 git init
-sudo apt-get install npm
+sudo apt-get install npm -y
 npm run prepare
 npm install -g npm
 npm install next
