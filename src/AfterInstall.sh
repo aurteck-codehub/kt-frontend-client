ef112e2f8d87d6a4fae2e1ef83271d8a25d92b63
Farman Ali <farmanali494917@gmail.com>
2023-04-22 22:44:47 +0500

    Update AfterInstall.sh

--- src/AfterInstall.sh
+++ src/AfterInstall.sh
@@ -4,16 +4,13 @@ export PATH="$PATH:/root/.nvm/versions/node/v18.16.0/bin/"
 sudo chown -R ubuntu:ubuntu /root/.nvm   
 sudo chmod -R 755 /root/.nvm
 sudo /root/.nvm/versions/node/v18.16.0/bin/npm install husky --save-dev
-sudo /usr/bin/git
 sudo /usr/bin/git init
-sudo /usr/bin/npm
-sudo /usr/local/bin/next
-sudo sudo /usr/bin/npm run prepare
+sudo /usr/local/bin/npm run prepare
 sudo /usr/local/bin/next dev
 sudo /usr/local/bin/next build
 sudo /usr/local/bin/next start
 sudo /usr/local/bin/npm run build
 sudo /usr/local/bin/chmod +x next
 sudo /usr/local/bin/npm install -g sass
-sudo /usr/local/bin/next/npm run build
+sudo /usr/local/bin/next build
 sudo /usr/local/bin/npm run dev
