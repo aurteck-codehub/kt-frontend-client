fe41fc2a4a9c9351751609820e009eb1e0312077
Farman Ali <farmanali494917@gmail.com>
2023-04-22 15:15:31 +0500

    Update and rename afterinstall.sh to AfterInstall.sh

--- AfterInstall.sh
+++ AfterInstall.sh
@@ -9,6 +9,7 @@ export NVM_DIR="/root/.nvm"
 /root/.nvm/versions/node/v18.16.0/bin/npm install
 /root/.nvm/versions/node/v18.16.0/bin/npm install next
 /root/.nvm/versions/node/v18.16.0/bin/npm install -g sass
+/root/.nvm/versions/node/v18.16.0/bin/npm run build
 /root/.nvm/versions/node/v18.16.0/bin/npm run dev
 
 
