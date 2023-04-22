2942c925a1ce85bd4c979555d4bdac16ccf5b78e
Farman Ali <farmanali494917@gmail.com>
2023-04-22 13:35:47 +0500

    Update afterinstall.sh

--- afterinstall.sh
+++ afterinstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
+cd /var/www/html/production
 export NVM_DIR="/root/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-cd /usr/share/nginx/html
 /root/.nvm/versions/node/v18.16.0/bin/npm install husky --save-dev
 /root/.nvm/versions/node/v18.16.0/bin/sudo apt-get install git -y
 /root/.nvm/versions/node/v18.16.0/bin/npm run prepare
