2df5e83cad071e24c893296594c29df96b26299c
Farman Ali <farmanali494917@gmail.com>
2023-04-22 15:33:36 +0500

    Update AfterInstall.sh

--- AfterInstall.sh
+++ AfterInstall.sh
@@ -8,11 +8,12 @@ export NVM_DIR="/root/.nvm"
 /root/.nvm/versions/node/v18.16.0/bin/git init
 /root/.nvm/versions/node/v18.16.0/bin/next dev
 /root/.nvm/versions/node/v18.16.0/bin/next build
-/root/.nvm/versions/node/v18.16.0/bin/next dev
+/root/.nvm/versions/node/v18.16.0/bin/next start
 /root/.nvm/versions/node/v18.16.0/bin/npm run build
 /root/.nvm/versions/node/v18.16.0/bin/npm install next
 /root/.nvm/versions/node/v18.16.0/bin/npm install -g sass
 /root/.nvm/versions/node/v18.16.0/bin/npm run build
 /root/.nvm/versions/node/v18.16.0/bin/npm run dev
+
 
 
