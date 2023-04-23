71cb365e7cbdadd6fa47dbc55a2a2fb42967f52c
Farman Ali <farmanali494917@gmail.com>
2023-04-23 22:35:46 +0500

    Update AfterInstall.sh

--- src/AfterInstall.sh
+++ src/AfterInstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /home/ubuntu/frontend
+cd /var/www/html/production
 export PATH="$PATH:/root/.nvm/versions/node/v18.16.0/bin/"
 sudo chown -R ubuntu:ubuntu /root/.nvm   
 sudo chmod -R 755 /root/.nvm
