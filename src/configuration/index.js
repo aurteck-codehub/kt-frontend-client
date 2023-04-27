6396d3ca57f55b6f9e2bc1848b53f0f8f0b71dec
Farman Ali <farmanali494917@gmail.com>
2023-04-27 23:39:43 +0500

    Update index.js

--- src/configuration/index.js
+++ src/configuration/index.js
@@ -1,6 +1,6 @@
 export const API_ROOT =
   process.env.NODE_ENV === "development"
-    ? "http://localhost:3000"
+    ? "http://localhost:4000"
     : "https://apiprod.signacapture.com";
 
 export const API_URL = 
