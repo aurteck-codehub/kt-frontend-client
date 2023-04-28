c55acc746e8910aea6f522bdbc78121f9e11a89f
Farman Ali <farmanali494917@gmail.com>
2023-04-28 17:37:01 +0500

    Update applicationstart.sh

--- src/applicationstart.sh
+++ src/applicationstart.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
 cd /var/www/html/production
- sudo yarn install --frozen-lockfile
- sudo npm install -g yarn
- sudo yarn add
- sudo yarn build
- sudo npm ci
-sudo yarn dev
-sudo yarn install react react-dom
+pm2 -f start kt-frontend-client
