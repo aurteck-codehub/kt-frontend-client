59c94fc0a30726e3619781c9c2bb675a7d471d43
Farman Ali <farmanali494917@gmail.com>
2023-04-29 21:23:46 +0500

    Update AfterInstall.sh

--- src/AfterInstall.sh
+++ src/AfterInstall.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
+sudo su
 cd /var/www/html/production
 export PATH="$PATH:/root/.nvm/versions/node/v19.9.0/bin/"
-chown -R ubuntu:ubuntu /root/.nvm 
+sudo chown -R ubuntu:ubuntu /root/.nvm 
 export PATH="$PATH:$(yarn global bin)"
-chmod -R 755 /root/.nvm
-/root/.nvm/versions/node/v18.16.0/bin/npm install husky --save-dev
-/usr/bin/git init
- /usr/local/bin/npm run prepare
- /usr/local/bin/next dev
- /usr/local/bin/next build
- /usr/local/bin/next start
- /usr/local/bin/npm run build
- /usr/local/bin/chmod +x next
- /usr/local/bin/npm install -g sass
- /usr/local/bin/next build
- /usr/local/bin/npm run dev
- npm install react react-dom
- yarn install --frozen-lockfile
+sudo chmod -R 755 /root/.nvm
+sudo /root/.nvm/versions/node/v18.16.0/bin/npm install husky --save-dev
+sudo /usr/bin/git init
+ sudo /usr/local/bin/npm run prepare
+ sudo /usr/local/bin/next dev
+ sudo /usr/local/bin/next build
+ sudo /usr/local/bin/next start
+ sudo /usr/local/bin/npm run build
+ sudo /usr/local/bin/chmod +x next
+ sudo /usr/local/bin/npm install -g sass
+ sudo /usr/local/bin/next build
+ sudo /usr/local/bin/npm run dev
+ sudo npm install react react-dom
+ sudo yarn install --frozen-lockfile
 
 
