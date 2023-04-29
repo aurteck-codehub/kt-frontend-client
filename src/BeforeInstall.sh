af60c9596c613a97814cebeee0229ebc62c95be5
Farman Ali <farmanali494917@gmail.com>
2023-04-29 23:40:11 +0500

    Update BeforeInstall.sh

--- src/BeforeInstall.sh
+++ src/BeforeInstall.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 cd /var/www/html/production
-sudo apt-get update
-sudo apt-get install -y build-essential libssl-dev libcairo2-dev libpango1.0-dev libjpeg-dev libgif-dev librsvg2-dev
-
-# Install global npm packages
-sudo npm install -g pm2
 sudo npm install
 
