0f120b81735db20ef67276ed92345112eb70f570
Farman Ali <farmanali494917@gmail.com>
2023-04-29 23:48:54 +0500

    Update AfterInstall.sh

--- src/AfterInstall.sh
+++ src/AfterInstall.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd /var/www/html/production
- export PATH="$PATH:/var/www/html/production/node_modules/.bin/"
- sudo next dev
- sudo  next build
+export PATH="$PATH:/var/www/html/production/node_modules/next/"
+ sudo npm dev
+ sudo  npm build
  
 
 
