6bbab14b517bc010b3c5d2473df0ef237eef0caf
Farman Ali <farmanali494917@gmail.com>
2023-04-30 02:31:18 +0500

    Update AfterInstall.sh

--- src/AfterInstall.sh
+++ src/AfterInstall.sh
@@ -2,7 +2,17 @@
 cd /var/www/html/production
 npm install sharp
 export PATH="$PATH:/usr/lib/node_modules/pm2/bin"
-pm2 start npm --name "kt-frontend-client" -- start
+
+pm2 describe "kt-backend-api"
+if [ $? -eq 0 ]
+then
+    pm2 restart "kt-frontend-client"
+else
+    pm2 start npm --name "kt-frontend-client" -- start
+fi
+pm2 startup systemd
+pm2 save
+
  
 
 
