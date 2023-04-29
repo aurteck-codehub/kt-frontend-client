2ef052597dbc5d7b9e8d73cde37e5058866bc013
Farman Ali <farmanali494917@gmail.com>
2023-04-29 22:16:35 +0500

    Update AfterInstall.sh

--- src/AfterInstall.sh
+++ src/AfterInstall.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 cd /var/www/html/production
  sudo npm run prepare
- 
-
  sudo npm run build
  
- sudo npm install -g sass
- 
- sudo npm run dev
- sudo npm install react react-dom
- sudo yarn install --frozen-lockfile
+
 
 
 
