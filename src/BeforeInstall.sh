f93fdb920a992164f82c2da7e3d456daccc4d40a
Farman Ali <farmanali494917@gmail.com>
2023-05-01 21:57:28 +0600

    Update BeforeInstall.sh

--- src/BeforeInstall.sh
+++ src/BeforeInstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
+
 sudo apt-get update -y
 cd /var/www/html/production
-export PATH=$PATH:/usr/lib/node_modules
 sudo npm install -g pm2
+sudo cp /usr/lib/node_modules/npm/package.json /var/www/html/production
 sudo npm install
-
