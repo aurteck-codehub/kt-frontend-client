a0a130835637cbe8c5acee00f1e8ed460876e5ac
Farman Ali <farmanali494917@gmail.com>
2023-05-01 22:40:30 +0600

    Update BeforeInstall.sh

--- src/BeforeInstall.sh
+++ src/BeforeInstall.sh
@@ -5,4 +5,6 @@ cd /var/www/html/production
 sudo npm install -g pm2
 curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
 sudo apt-get install -y nodejs
+rm -f package-lock.json
+sudo npm install
 
