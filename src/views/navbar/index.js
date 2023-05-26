46a3a4c6de600e3ce45aa2c7b252997b8ac769ff
arslan456 <muhammadarslanjabbar@gmail.com>
2023-05-26 23:03:41 +0500

    'mobile-slides'

--- src/views/navbar/index.js
+++ src/views/navbar/index.js
@@ -170,11 +170,6 @@ const Navbar = () => {
                     </Tooltip>
                   </>
                 ) : (
-                  // <NextLink href="/login">
-                  //   <IconButton size="medium">
-                  //     <PersonOutlineIcon sx={{ color: "custom.green" }} />
-                  //   </IconButton>
-                  // </NextLink>
                   <Box sx={{display: 'flex'}}>
                   <Box sx={{ px: 0, paddingRight: '15px' }}>
                       <NextLink
